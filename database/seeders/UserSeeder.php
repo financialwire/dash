02e9b40278406163bb09bd74f4fc9d42ce3ebb55
vitorbizarra <vitorbizarracontato@gmail.com>
2024-03-22 20:20:04 -0300

    :card_file_box: raw: DatabaseSeeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        \App\Models\User::factory()->create([
+            'name' => 'Foo Bar',
+            'email' => 'foo@bar.com',
+        ]);
+    }
+}
