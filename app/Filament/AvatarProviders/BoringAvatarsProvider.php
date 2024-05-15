aaa48005f06f75837457dd8418ce99abf7687f1c
vitorbizarra <vitorbizarracontato@gmail.com>
2024-05-14 22:02:03 -0300

    :sparkles: feat: BoringAvatars

---
+++ app/Filament/AvatarProviders/BoringAvatarsProvider.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Filament\AvatarProviders;
+
+use Filament\AvatarProviders\Contracts;
+use Filament\Facades\Filament;
+use Illuminate\Contracts\Auth\Authenticatable;
+use Illuminate\Database\Eloquent\Model;
+
+class BoringAvatarsProvider implements Contracts\AvatarProvider
+{
+    public function get(Model | Authenticatable $record): string
+    {
+        $name = str(Filament::getNameForDefaultAvatar($record))
+            ->trim()
+            ->explode(' ')
+            ->map(fn (string $segment): string => filled($segment) ? mb_substr($segment, 0, 1) : '')
+            ->join(' ');
+
+        return 'https://source.boringavatars.com/beam/120/' . urlencode($name) . '?colors=5b21b6,36e05a,0c0c0c';
+    }
+}
--- app/Providers/Filament/DashPanelProvider.php
+++ app/Providers/Filament/DashPanelProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers\Filament;
 
+use App\Filament\AvatarProviders\BoringAvatarsProvider;
 use App\Filament\Pages\Auth\EditProfile;
 use App\Filament\Pages\Dashboard;
 use App\Filament\Widgets;
@@ -36,6 +37,7 @@ class DashPanelProvider extends PanelProvider
             ->colors([
                 'primary' => Color::Purple,
             ])
+            ->defaultAvatarProvider(BoringAvatarsProvider::class)
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
             ->pages([
