64eaaf3cde072d6fb3c917c5c149e4e3a71cf465
vitorbizarra <vitorbizarracontato@gmail.com>
2024-05-22 21:37:56 -0300

    :sparkles: feat: Email de boas vindas

---
+++ app/Notifications/UserCreated.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Notifications;
+
+use App\Models\User;
+use App\Providers\Filament\DashPanelProvider;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class UserCreated extends Notification
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct(
+        protected User $user
+    ) {
+        //
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail(): MailMessage
+    {
+        $appName = config('app.name');
+
+        $name = str($this->user->name)->explode(' ')->first();
+
+        return (new MailMessage)
+            ->subject("Bem-vindo(a) ao {$appName}!")
+            ->greeting("Olá, {$name}!")
+            ->line("Seja bem-vindo(a) ao {$appName}! Obrigado por criar sua conta conosco!")
+            ->action('Fazer login', filament()->getPanel('dash')->getLoginUrl())
+            ->salutation(' ');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            //
+        ];
+    }
+}
